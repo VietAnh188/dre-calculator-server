b86206599bbd5b8da914ea15ccc3e7f41fc549d5
VietAnh188 <anhkun123456@gmail.com>
2022-06-02 19:05:30 +0700

    install cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 1234
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
+
+app.use(cors())
 
 app.use('/', router)
 
